fb067500777106fdc7b6511661cb313c3bb78a62
shira <shira@gmail.ocm>
2023-04-30 12:26:24 +0300

    Change the layout of 2 box's sections

--- pages/day1.jsx
+++ pages/day1.jsx
@@ -5,12 +5,13 @@ export default function Day1() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1>Fix the Website Challenge</h1>
+        <h1>Fix the Website Challenge - Shira</h1>
       </header>
       <main className={styles.main}>
         <section className={styles.section}>
           <h2>Section 1</h2>
-          <div className={styles.firstSection}>
+          <div className={styles.columnSection}>
+            <div className={styles.box}>Box 1</div>
             <div className={styles.box}>Box 1</div>
             <div className={styles.box}>Box 2</div>
             <div className={styles.box}>Box 3</div>
@@ -19,11 +20,10 @@ export default function Day1() {
         </section>
         <section className={styles.section}>
           <h2>Section 2</h2>
-          <div className={styles.row}>
+          <div className={styles.rowSection}>
+            <div className={styles.box}>Box 5</div>
             <div className={styles.box}>Box 5</div>
             <div className={styles.box}>Box 6</div>
-          </div>
-          <div className={styles.row}>
             <div className={styles.box}>Box 7</div>
             <div className={styles.box}>Box 8</div>
           </div>

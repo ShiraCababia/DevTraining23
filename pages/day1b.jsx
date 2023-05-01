360e0694f09e5e10fb2e54fb81a57a04141d8f6c
shira <shira@gmail.ocm>
2023-05-01 09:55:41 +0300

    Start working on navbar

--- pages/day1b.jsx
+++ pages/day1b.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import moshiko from './day1b.module.css';
 
 export default function Day1b() {
   return (
-    <div>
+    <div className={moshiko.container}>
       <h1>Day 1 Bonus Challenge</h1>
+      <div className={moshiko.navBar}>
+        <div className={moshiko.navBarItem}>Blog</div>
+        <div className={moshiko.navBarItem}>Work</div>
+        <div className={moshiko.navBarItem}>About</div>
+        <div className={moshiko.navBarItem}>Contact</div>
+      </div>
     </div>
   );
 }
